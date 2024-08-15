db5f650137e95371bafdf57c3e45c70de7419939
EricImperiale <ericimperiale@hotmail.com>
2024-08-15 11:45:50 -0300

    Se agregan modificaciones

--- resources/views/owners/index.blade.php
+++ resources/views/owners/index.blade.php
@@ -2,11 +2,8 @@
 /** @var \App\Models\Owner[]|\Illuminate\Database\Eloquent\Collection $owners */
 /** @var \App\Searches\BaseSearches $baseSearches */
 ?>
-<x-app-layout
-    title="Tus Propietarios"
->
+<x-app-layout title="Tus Propietarios">
     <div class="container mx-auto">
-
         <x-auth-message />
 
         <div class="flex items-center justify-between mb-4">
@@ -17,7 +14,7 @@
             </a>
         </div>
 
-        @if($owners)
+        @if($owners->isNotEmpty() || $baseSearches->getFullName())
             <x-base-filter
                 route="owners.index"
                 name="fn"
@@ -27,36 +24,35 @@
             />
         @endif
 
-        @if($baseSearches->getFullName())
-            <p class="mb-3">Se muestran los resultados para la búsqueda del propietario <b>{{ $baseSearches->getFullName() }}</b>.</p>
-        @endif
+        @if($owners->isEmpty() && $baseSearches->getFullName())
+            <p class="text-gray-700">No se encontraron propietarios con el filtro aplicado <b>{{ $baseSearches->getFullName() }}</b>.</p>
+        @elseif($owners->isEmpty())
+            <p class="text-gray-700">Aún no hay propietarios cargados.</p>
+        @else
+            @if($baseSearches->getFullName())
+                <p class="mb-3">Se muestran los resultados para la búsqueda del propietario <b>{{ $baseSearches->getFullName() }}</b>.</p>
+            @endif
 
-        @if($owners->isNotEmpty())
             <div class="overflow-x-auto">
-                <x-table
-                    :info="$owners"
-                    model="owners"
-                >
+                <x-table :info="$owners" model="owners">
                     <thead>
-                        <tr>
-                            <th class="px-4 py-2 text-left">Nombre Completo</th>
-                            <th class="px-4 py-2 text-left">DNI</th>
-                            <th class="px-4 py-2 text-left">Email</th>
-                            <th class="px-4 py-2 text-left">Dirección Completa</th>
-                            <th class="px-4 py-2 text-left">Número de Teléfono</th>
-                            @if(auth()->user()->has_permission)
-                                <th class="px-4 py-2 text-left">Acciones</th>
-                            @endif
-                        </tr>
+                    <tr>
+                        <th class="px-4 py-2 text-left">Nombre Completo</th>
+                        <th class="px-4 py-2 text-left">DNI</th>
+                        <th class="px-4 py-2 text-left">Email</th>
+                        <th class="px-4 py-2 text-left">Dirección Completa</th>
+                        <th class="px-4 py-2 text-left">Número de Teléfono</th>
+                        @if(auth()->user()->has_permission)
+                            <th class="px-4 py-2 text-left">Acciones</th>
+                        @endif
+                    </tr>
                     </thead>
                 </x-table>
             </div>
-        @else
-            <p class="text-gray-700">Aún no hay propietarios cargados.</p>
-        @endif
 
-        <div class="mt-4">
-           {{ $owners->links() }}
-        </div>
+            <div class="mt-4">
+                {{ $owners->links() }}
+            </div>
+        @endif
     </div>
 </x-app-layout>
